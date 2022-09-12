35f4e1c5403789a9052df6c49a62a5956a45c80b
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-12 10:20:43 +0100

    Update streamlit_app.py
    added images to the food items and added smoothie to bottom

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit
 streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('\N{chicken}  Hard-Boiled Free-Range Egg \N{chicken} ')
+streamlit.text('\N{Oatmeal} Omega 3 & Blueberry Oatmeal')
+streamlit.text('\N{kale} Kale, Spinach & Rocket Smoothie')
+streamlit.text('\N{chicken}  Hard-Boiled Free-Range Egg')
+
+streamlit.header('\N{banana} \N{strawberry} Build your Own Fruit Smoothie \N{Kiwi}\n{grapes}')
