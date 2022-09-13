d44207aacc5120cd26d3c0bdaff265de3fb28130
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 20:41:38 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -57,8 +57,9 @@ try:
 		streamlit.error("Please select a fruit to get information.")
 	else:
 		back_from_function = get_fruityvice_data(fruit_choice)
-		steamlit.dataframe(back_from_function)         
-except URLError as e:
+		steamlit.dataframe(back_from_function)    
+		
+#except URLError as e:
 #streamlit.error()
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
