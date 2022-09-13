82482de7887b690b0bc94544719248fa88892684
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 19:47:29 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,8 +28,11 @@ streamlit.header("Fruityvice Fruit Advice!")
 #Lesson 9 python 
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
-#streamlit.header("Fruityvice Fruit Advice!")
+
+# write your own comment -what does the next line do? 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  #kust writes the data to the screen
+# write your own comment - what does this do?
+streamlit.dataframe(fruityvice_normalized)
 
 
 #try:
