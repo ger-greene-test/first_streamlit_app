bd828e3e1bb9848536a19eee9c6e6f5bb9ae225e
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 20:28:17 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,8 @@ try:
      fruitvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
      streamlit.dataframe(fruityvice_normalized)
-expect URLError as e:
+    
+except URLError as e:
     streamlit.error()
 
 #import requests
