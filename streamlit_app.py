66057581a8e84137c19a7ac11eb7e56b6542da5f
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 16:23:35 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,28 +13,46 @@ streamlit.text('\N{chicken}  Hard-Boiled Free-Range Egg')
 
 streamlit.header('\N{banana}  Build your Own Fruit Smoothie' )
 
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show  = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
-
-
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-# takes the json version of the reponse and normalize it 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# output it the screen as a table
-streamlit.dataframe(fruityvice_normalized)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+#New section to display fruitvice api response 
+streamlit.header('Fruitvice Fruit Advice!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+        streamlit.error("Please sekect a fruit to get information.")
+    else:
+      fruitvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      streamlit.dataframe(fruityvice_normalized)
+
+
+
+
+
+#import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
 #my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+# takes the json version of the reponse and normalize it 
+
+# output it the screen as a table
+
+
+
+
 
 streamlit.dataframe(fruits_to_show)
 #dont run anything past here while we troubleshoot
