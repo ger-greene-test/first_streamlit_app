86f7a135f90de1953f9a0374b9ce8949eaeb0a0d
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 20:39:49 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,21 +46,20 @@ streamlit.header("Fruityvice Fruit Advice!")
 #create the repeatable coe block (called a function)
 def get_fruityvice_data(this_fruit_choice):
 	fruitvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-     	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-     	return fruityvice_normalized
+	fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+	return fruityvice_normalized
 
 #New section to display API RESPONSE 
 streamlit.header('Fruityvice Fruit Advice!!')
 try:
-    fruit_choice = streamlit.text_input('What fruit would you like information about?')
-    if not fruit_choice:
-       streamlit.error("Please select a fruit to get information.")
-    else:
-	  back_from_function = get_fruityvice_data(fruit_choice)
-	  steamlit.dataframe(back_from_function)         
+	fruit_choice = streamlit.text_input('What fruit would you like information about?')
+	if not fruit_choice:
+		streamlit.error("Please select a fruit to get information.")
+	else:
+		back_from_function = get_fruityvice_data(fruit_choice)
+		steamlit.dataframe(back_from_function)         
 except URLError as e:
-        streamlit.error()
-
+streamlit.error()
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
