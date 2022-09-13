574161080eafc08e29ada1be166ec6e8a538a0ec
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 20:41:02 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -59,7 +59,7 @@ try:
 		back_from_function = get_fruityvice_data(fruit_choice)
 		steamlit.dataframe(back_from_function)         
 except URLError as e:
-streamlit.error()
+#streamlit.error()
 #import requests
 #fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
