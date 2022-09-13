c2ebaf1debfe4d3b2ab7fb9edcad0490918cb6f7
MellyMel Melnyk <113340614+ger-greene-test@users.noreply.github.com>
2022-09-13 20:23:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,17 +27,30 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 #Lesson 9 python 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
 
+##New new
+#New Section to display fruityvice api response
+streamlit.header('Fruityvice Fruit Advice!!')
+try:
+    fruit_choice = streamlit.text_input('What fruit would you like information about?')
+    if not fruit_choice:
+       streamlit.error("Please select a fruit to get information.")
+   else:
+    fruitvice_response = request.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+     streamlit.dataframe(fruityvice_normalized)
+expect URLError as e:
+streamlit.error()
 
 #import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  #kust writes the data to the screen
+#fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())  #kust writes the data to the screen
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
 
 streamlit.stop()
 #bolt in snowflake
